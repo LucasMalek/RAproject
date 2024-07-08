10825edaff9a620ae93454c0b55e977d22421703
Lucas <lucasmmo2010@hotmail.com>
2024-07-08 19:13:40 -0300

    Only update is missing

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import os
 import shutil
 from datetime import timedelta
 import glob
+import sqlite3
+from io import BytesIO
+from werkzeug.datastructures import FileStorage
 
 
 def limpar_arquivos_session():
@@ -42,36 +45,49 @@ requisition_queue = queue.Queue()
 result_queue = queue.Queue()
 
 
-def reform_list(string):
-    padrao_relations_tuples = r'[A-Z][a-zA-Z]+(?=\()'
+def reform_list(string, requisition):
+    padrao_relations_tuples = r'[^\s]*(?=\()'
+    padrao_relations_label = r'[^\s]*\([^)]+\)'
     relations_tuples = []
     relation_label = []
-    padrao_relations_label = r'[A-Z][a-zA-Z]+\([^)]+\)'
     indice = 0
+    
     while indice < len(string):
         match_encontrado = False
         match = re.search(padrao_relations_label, string[indice:])
         if match:
-            relation_label.append(match.group())
-            indice += match.end()
-            match_encontrado = True
+            identified = match.group()
+            if requisition != 'list':
+               if requisition in identified:
+                    relation_label.append(identified)
+                    break
+               else: 
+                indice += match.end()
+                match_encontrado = True
+            else:
+                relation_label.append(identified)
+                indice += match.end()
+                match_encontrado = True
         if not match_encontrado:
             break
+        
     for i in relation_label:
-        requisition_queue.put(
-            [re.search(padrao_relations_tuples, i).group()+';', session['session_init'], 2])
-        relations_tuples.append(result_queue.get())
-
+            requisition_queue.put(
+                [re.search(padrao_relations_tuples, i).group()+';', session['session_init'], 2])
+            relations_tuples.append(result_queue.get())
+            
     for i, item in enumerate(relations_tuples):
         lines = item.strip().splitlines()
-        padrao = re.compile(r'^\s*\d+,\s*[^,]+(,\s*[^,]+)*\s*$')
+        padrao = re.compile(r'^\s*[^,]+(?:,\s*[^,]+)*\s*$')
         tuplas = []
-        for line in lines:
-            if padrao.match(line):
-                tupla = tuple(part.strip() for part in line.split(','))
-                tuplas.append(tupla)
+        for line in lines[1:]:
+            if not line.startswith('-') and 'tuple returned' not in line:
+                if padrao.match(line):
+                    parts = [part.strip() for part in line.split(',')]
+                    tupla = tuple(parts)
+                    tuplas.append(tupla)
         relations_tuples[i] = tuplas
-
+    
     return relation_label, relations_tuples
 
 
@@ -181,11 +197,6 @@ def CreateConsultfromOperators(operators):
     return query_sufix
 
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
-
-
 @app.route('/consult', methods=['POST'])
 async def consult():
     if 'session_init' in session:
@@ -205,12 +216,12 @@ async def consult():
 @app.route('/loadfile', methods=['GET', 'POST'])
 def loadfile():
     if 'session_init' not in session:
-        relations_details_structure = []
-        file = request.files['file']
-        bd_name = file.filename
-        file.save(os.path.join(os.getcwd(), bd_name))
         global count
         session['session_init'] = count
+        relations_details_structure = []
+        file = request.files['file']
+        bd_name = f"{str(session['session_init'])}.db"
+        file.save(os.path.join(os.getcwd(), bd_name))
         requisition_queue.put(
             [ralib.RA(bd_name), session['session_init'], 1])
         requisition_queue.put(
@@ -220,7 +231,7 @@ def loadfile():
         try:
             requisition_queue.put(['\list;', session['session_init'], 2])
             result = result_queue.get()
-            relations_labels, tuples = reform_list(result)
+            relations_labels, tuples = reform_list(result, 'list')
             for item in relations_labels:
                 match = re.match(r'(\w+)\((.*?)\)', item)
                 if match:
@@ -240,6 +251,135 @@ def prototipo():
     return render_template('prototipohome.html')
 
 
+@app.route('/colectinfofromtable', methods=['POST'])
+def colectinfofromtable():
+    if 'session_init' in session:
+        requisition = request.get_json()
+        tablename = requisition['tablename']
+        conn = sqlite3.connect(f"{str(session['session_init'])}.db")
+        cursor = conn.cursor()
+        cursor.execute(f"PRAGMA table_info({tablename})")
+        rows = cursor.fetchall()
+        columns = [row[1] for row in rows]
+        types = [row[2] for row in rows]
+        info = {}
+        if(requisition['type'] == 'includetuples'):
+            tuples = []
+            for row in rows:
+                result_row = []
+                for value in row:
+                    result_row.append(value)
+                tuples.append(result_row)
+            info = {
+                "attributes": columns,
+                "types": types,
+                "tuples": tuples
+            }
+        else:
+            info = {
+                "attributes": columns,
+                "types": types
+            }
+        return jsonify(info)
+
+
+@app.route('/insert', methods=['POST'])
+def adddataontable():
+    if 'session_init' in session:
+        tablename = ''
+        relations_details_structure = []
+        conn = sqlite3.connect(f"{str(session['session_init'])}.db")
+        cursor = conn.cursor()
+        tuples = request.get_json()
+        for table, info in tuples.items():
+            tablename = info['tablename']
+            for tuple in info["tuples"]:
+                placeholders = ", ".join(["?"] * len(tuple))
+                cursor.execute(
+                    f"INSERT INTO {tablename} VALUES ({placeholders})", tuple)
+        conn.commit()
+        conn.close()
+        try:
+            requisition_queue.put([f"radb {str(session['session_init'])}.db", session['session_init'], 2])
+            result_queue.get()
+            requisition_queue.put(['\list;', session['session_init'], 2])
+            result = result_queue.get()
+            relations_labels, tuples = reform_list(result, tablename)
+            for item in relations_labels:
+                match = re.match(r'(\w+)\((.*?)\)', item)
+                if match:
+                    nome_relacao = match.group(1).strip()
+                    atributos_str = match.group(2)
+                    atributos = [atributo.strip()
+                                 for atributo in atributos_str.split(',')]
+                    relations_details_structure.append(
+                        [nome_relacao] + atributos)
+            return jsonify([tuples, relations_details_structure])
+        except Exception as e:
+            return jsonify({'erro': e})
+
+
+@app.route('/createdbfile', methods=['POST'])
+def createdbfile():
+    if 'session_init' not in session:
+        global count
+        session['session_init'] = count
+        relations_details_structure = []
+        conn = sqlite3.connect(f"{str(session['session_init'])}.db")
+        cursor = conn.cursor()
+        db = request.get_json()
+        for table, info in db.items():
+            tablename = info["tablename"]
+            attr_types = ", ".join([f"{attribute} {typ}" for attribute, typ in zip(
+                info["attributes"], info["types"])])
+            cursor.execute(f"CREATE TABLE {tablename} ({attr_types})")
+            for tuple in info["tuples"]:
+                placeholders = ", ".join(["?"] * len(tuple))
+                cursor.execute(
+                    f"INSERT INTO {tablename} VALUES ({placeholders})", tuple)
+        conn.commit()
+        conn.close()
+        bd_name = f"{str(session['session_init'])}.db"
+        requisition_queue.put(
+            [ralib.RA(bd_name), session['session_init'], 1])
+        requisition_queue.put(
+            [f"radb {bd_name}", session['session_init'], 2])
+        result_queue.get()
+        count += 1
+        try:
+            requisition_queue.put(['\list;', session['session_init'], 2])
+            result = result_queue.get()
+            relations_labels, tuples = reform_list(result, 'list')
+            for item in relations_labels:
+                match = re.match(r'(\w+)\((.*?)\)', item)
+                if match:
+                    nome_relacao = match.group(1).strip()
+                    atributos_str = match.group(2)
+                    atributos = [atributo.strip()
+                                 for atributo in atributos_str.split(',')]
+                    relations_details_structure.append(
+                        [nome_relacao] + atributos)
+            return jsonify([tuples, relations_details_structure])
+        except Exception as e:
+            return jsonify({'erro': e})
+  
+
+@app.route('/deletetable', methods = ['POST'])
+def deletetable():
+     if 'session_init' in session:
+        conn = sqlite3.connect(f"{str(session['session_init'])}.db")
+        cursor = conn.cursor()
+        tablename = request.get_json()['tablename']
+        cursor.execute(f"DELETE FROM {tablename}")
+        conn.commit()
+        conn.close()
+        return 'ok', 200
+    
+    
+@app.route('/updatetable', methods = ['POST'])
+def updatetable():
+    return
+     
 atexit.register(limpar_arquivos_session)
 atexit.register(clean_db_files)
 
